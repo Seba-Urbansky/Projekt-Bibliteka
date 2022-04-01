6042740c8502468703e03b16376e35cfcab6a035
Seba-Urbansky <68894278+Seba-Urbansky@users.noreply.github.com>
2022-04-01 08:13:44 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 
 // http://fizyka.umk.pl/~leii/wskaznikiStrukturyAiR.pdf
 
-struct klient{
+struct klient
+{
 
     int numer_karty;
     char imie[MAX];
@@ -18,110 +19,165 @@ struct klient{
 
     struct klient *poprzedni, *nastepny;
 
-}baza;
+} baza;
 
 struct klient *wpis;
 struct klient *poczatek = NULL;
 struct klient *koniec = NULL;
 
-void menu_klientow() {
-    int wybor;
+void wyswietl_baze_klientow();
+void dodaj_klienta();
+void edytuj_klienta();
+void usun_klienta();
+void menu();
+void menu_klientow();
+void wyjscie();
+void wczytaniepliku();
 
-printf("Witamy w bibliotece, baza klientow! Co chcesz zrobic?");
-printf("\n\n");
-printf("1. Wyswietl baze klientow");
-printf("\n");
-printf("2. Dodaj klienta");
-printf("\n");
-printf("3. Edytuj klienta");
-printf("\n");
-printf("4. Usun klienta");
-printf("\n");
-printf("5. Powrot do menu");
-printf("\n");
-printf("6. Wyjscie z programu");
-printf("\n\n");
+void wyswietl_baze_klientow()
+{
 
-scanf("%d", &wybor);
-printf("\n");
-
-switch (wybor) {
-case 1:
-    wyswietl_baze_klientow();
-    break;
-
-case 2:
-    dodaj_klienta();
-    break;
-
-case 3:
-    edytuj_klienta();
-    break;
-
-case 4:
-    usun_klienta();
-    break;
-
-case 5:
-    menu();
-    break;
-
-case 6:
-    wyjscie();
-    break;
-
-default:
-    printf("Niepoprawna instrukcja");
-    break;
-}
-}
-
-void menu() {
-    int wybor;
-
-printf("Witamy w bibliotece! Co chcesz zrobic?");
-printf("\n\n");
-printf("1. Otwieranie bazy klientow");
-printf("\n");
-printf("2. Otwieranie bazy ksiazek");
-printf("\n");
-printf("3. Otwieranie bazy wypozyczen");
-printf("\n");
-printf("4. Wyjscie z programu");
-printf("\n\n");
-
-scanf("%d", &wybor);
-printf("\n");
-
-switch (wybor) {
-case 1:
+    for (; NULL != wpis ; wpis =  wpis -> nastepny )
+        printf ("%d ", wpis->numer_karty );
+    printf ("%d ", wpis->imie);
+    printf ("%d ", wpis->nazwisko);
+    printf ("%d ", wpis->telefon);
+    printf ("%d ", wpis->email);
+    printf ("\n");
     menu_klientow();
-    break;
-
-case 2:
-    //todo
-    printf("To na kamien milowy 2");
-    break;
-
-case 3:
-    //todo
-    printf("To na kamien milowy 2");
-    break;
-
-case 4:
-    wyjscie();
-    break;
-
-default:
-    printf("Niepoprawna instrukcja");
-    break;
-
-}
 }
 
-void wyjscie() {
-printf("Zamykanie programu...");
-exit(0);
+void dodaj_klienta()
+{
+
+    struct klient *wpis = (struct klient *)malloc(sizeof(baza));
+
+
+    printf("Podaj numer karty: \n");
+    scanf("%s",&wpis->numer_karty);
+
+    printf("Podaj imie: \n");
+    scanf("%s",&wpis->imie);
+
+    printf("Podaj nazwisko: \n");
+    scanf("%s",&wpis->nazwisko);
+
+    printf("Podaj telefon: \n");
+    scanf("%s",&wpis->telefon);
+
+    printf("Podaj email: \n");
+    scanf("%s",&wpis->email);
+
+    menu_klientow();
+}
+
+void edytuj_klienta() {;}
+
+void usun_klienta() {;}
+
+void menu_klientow()
+{
+    int wybor;
+
+    printf("Witamy w bibliotece, baza klientow! Co chcesz zrobic?");
+    printf("\n\n");
+    printf("1. Wyswietl baze klientow");
+    printf("\n");
+    printf("2. Dodaj klienta");
+    printf("\n");
+    printf("3. Edytuj klienta");
+    printf("\n");
+    printf("4. Usun klienta");
+    printf("\n");
+    printf("5. Powrot do menu");
+    printf("\n");
+    printf("6. Wyjscie z programu");
+    printf("\n\n");
+
+    scanf("%d", &wybor);
+    printf("\n");
+
+    switch (wybor)
+    {
+    case 1:
+        wyswietl_baze_klientow();
+        break;
+
+    case 2:
+        dodaj_klienta();
+        break;
+
+    case 3:
+        edytuj_klienta();
+        break;
+
+    case 4:
+        usun_klienta();
+        break;
+
+    case 5:
+        menu();
+        break;
+
+    case 6:
+        wyjscie();
+        break;
+
+    default:
+        printf("Niepoprawna instrukcja");
+        break;
+    }
+}
+
+void menu()
+{
+    int wybor;
+
+    printf("Witamy w bibliotece! Co chcesz zrobic?");
+    printf("\n\n");
+    printf("1. Otwieranie bazy klientow");
+    printf("\n");
+    printf("2. Otwieranie bazy ksiazek");
+    printf("\n");
+    printf("3. Otwieranie bazy wypozyczen");
+    printf("\n");
+    printf("4. Wyjscie z programu");
+    printf("\n\n");
+
+    scanf("%d", &wybor);
+    printf("\n");
+
+    switch (wybor)
+    {
+    case 1:
+        menu_klientow();
+        break;
+
+    case 2:
+        //todo
+        printf("To na kamien milowy 2");
+        break;
+
+    case 3:
+        //todo
+        printf("To na kamien milowy 2");
+        break;
+
+    case 4:
+        wyjscie();
+        break;
+
+    default:
+        printf("Niepoprawna instrukcja");
+        break;
+
+    }
+}
+
+void wyjscie()
+{
+    printf("Zamykanie programu...");
+    exit(0);
 }
 
 /*|||||||||||||||||||||||||||||||||||||||||||||||||||||*/
@@ -129,81 +185,55 @@ exit(0);
 /*|||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 //klienci
 
-void wczytaniepliku() {
-FILE *plik;
-plik = fopen("klienci.csv", "r");
+void wczytaniepliku()
+{
+    FILE *plik;
+    plik = fopen("klienci.csv", "r");
 
-while (!feof (plik)) {
-wpis = malloc(sizeof(baza));
-
-fscanf(plik, "%d", wpis->numer_karty);
-fscanf(plik, "%s", wpis->imie);
-fscanf(plik, "%s", wpis->nazwisko);
-fscanf(plik, "%s", wpis->telefon);
-fscanf(plik, "%s", wpis->email);
-
-wpis -> nastepny = NULL;
-}
-}
-
-void zapispliku(struct klienci *wezel) {
-FILE *plik;
-plik = fopen("klienci.csv", "r");
-
-while (!feof (plik)) {
-wpis = malloc(sizeof(baza));
-
-fprintf(plik, "%d", wpis->numer_karty);
-fprintf(plik, "%s", wpis->imie);
-fprintf(plik, "%s", wpis->nazwisko);
-fprintf(plik, "%s", wpis->telefon);
-fprintf(plik, "%s", wpis->email);
-
-wpis -> nastepny = NULL;
-}
+    struct klient *poprzedni;
+    while (!feof (plik))
+    {
+        wpis = malloc(sizeof(baza));
+        fscanf(plik, "%d %s %s %s %s", &wpis->numer_karty, &wpis->imie, &wpis->nazwisko, &wpis->telefon, &wpis->email);
+        if (poczatek == NULL) {
+            poczatek = wpis;
+            poprzedni = wpis;
+        } else if (wpis != NULL) {
+            poprzedni->nastepny = wpis;
+            wpis->poprzedni = poprzedni;
+            poprzedni = wpis;
+        }
+    }
+    if (wpis != NULL) {
+        koniec = wpis;
+    }
 }
 
-void wyswietl_baze_klientow() {
+void zapispliku(struct klient *wezel)
+{
+    FILE *plik;
+    plik = fopen("klienci.csv", "r");
 
-for (; NULL != wpis ; wpis =  wpis -> nastepny )
-printf ("%d ", wpis->numer_karty );
-printf ("%d ", wpis->imie);
-printf ("%d ", wpis->nazwisko);
-printf ("%d ", wpis->telefon);
-printf ("%d ", wpis->email);
-printf ("\n");
-menu_klientow();
+    while (!feof (plik))
+    {
+        wpis = malloc(sizeof(baza));
+
+        fprintf(plik, "%d", wpis->numer_karty);
+        fprintf(plik, "%s", wpis->imie);
+        fprintf(plik, "%s", wpis->nazwisko);
+        fprintf(plik, "%s", wpis->telefon);
+        fprintf(plik, "%s", wpis->email);
+
+        wpis -> nastepny = NULL;
+    }
 }
 
 
-
-void dodaj_klienta() {
-
-struct klient *wpis = (struct klient *)malloc(sizeof(baza));
-
-
-printf("Podaj numer karty: \n");
-scanf("%s",&wpis->numer_karty);
-
-printf("Podaj imie: \n");
-scanf("%s",&wpis->imie);
-
-printf("Podaj nazwisko: \n");
-scanf("%s",&wpis->nazwisko);
-
-printf("Podaj telefon: \n");
-scanf("%s",&wpis->telefon);
-
-printf("Podaj email: \n");
-scanf("%s",&wpis->email);
-
-menu_klientow();
-}
-void edytuj_klienta() {;}
-void usun_klienta() {;}
-
-
-int main() {
-wczytaniepliku();
-menu();
+int main()
+{
+    wczytaniepliku();
+    printf("Poczatek: %s\n", poczatek->imie);
+    printf("Nastepny: %s\n", poczatek->nastepny->imie);
+    printf("Koniec: %s\n", koniec->imie);
+    menu();
 }

18333b255d3fa4fb60b79c4ed2e86ad3f4efa4f8
Sebastian <sebau188@gmail.com>
2022-04-30 22:14:02 +0200

    hi

--- wypozyczenia.c
+++ wypozyczenia.c
@@ -7,9 +7,7 @@
 #include "wypozyczenia.h"
 #include "ksiazki.h"
 #include <time.h>
-const int GODZINA = 3600;
-const int DZIEN = 24 * GODZINA;
-const int TYDZIEN = 7 * DZIEN;
+
 // Wypożyczanie/oddawanie książek (kto wypożyczył (numer karty), którą książkę (ID), kiedy, do kiedy).
 // http://fizyka.umk.pl/~leii/wskaznikiStrukturyAiR.pdf
 

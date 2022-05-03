feb618e903176778f42f70af4556ccb6bc37b9c6
Sebastian <sebau188@gmail.com>
2022-05-03 22:31:08 +0200

    hi

--- ksiazki.h
+++ ksiazki.h
@@ -46,8 +46,8 @@ int wyszukaj_ksiazki_po_roku();
 Ksiazka* zamien(Ksiazka* a, Ksiazka* b);
 int policz_ksiazki();
 void sortowanie(enum SortowanieKolejnosc kolejnosc, enum SortowanieAtrybut atrybut);
-
-
+enum SortowanieKolejnosc {Rosnaca = 1, Malejaca = 0}; 
+enum SortowanieAtrybut {Tytul = 0, Autor = 1};
 
 
 

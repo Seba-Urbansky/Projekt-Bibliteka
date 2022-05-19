297e37adca9c9c1aef01844197368e72c2abbaf4
Sebastian <sebau188@gmail.com>
2022-05-19 16:16:05 +0200

    hi

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -3,5 +3,5 @@ var searchData=
   ['do_5fkiedy_0',['do_kiedy',['../wypozyczenia_8h.html#ac576a9dac5ce2f6e51b45ceef23778d9',1,'wypozyczenia.h']]],
   ['dodaj_5fklienta_1',['dodaj_klienta',['../klienci_8c.html#ab0f0128ac537d0a9c981f44d93517ec9',1,'dodaj_klienta():&#160;klienci.c'],['../klienci_8h.html#ab0f0128ac537d0a9c981f44d93517ec9',1,'dodaj_klienta():&#160;klienci.c']]],
   ['dodaj_5fksiazke_2',['dodaj_ksiazke',['../ksiazki_8h.html#a18018b143b960db33a39ebcaa5c6c06c',1,'dodaj_ksiazke():&#160;ksiazki.c'],['../ksiazki_8c.html#a18018b143b960db33a39ebcaa5c6c06c',1,'dodaj_ksiazke():&#160;ksiazki.c']]],
-  ['dodaj_5fwypozyczenie_3',['dodaj_wypozyczenie',['../wypozyczenia_8h.html#ab4d7bce300ef21148db92a04bf30dfc8',1,'dodaj_wypozyczenie():&#160;wypozyczenia.c'],['../wypozyczenia_8c.html#ab4d7bce300ef21148db92a04bf30dfc8',1,'dodaj_wypozyczenie():&#160;wypozyczenia.c']]]
+  ['dodaj_5fwypozyczenie_3',['dodaj_wypozyczenie',['../wypozyczenia_8h.html#aaac626409a07d96baa8968071ea8ecf2',1,'dodaj_wypozyczenie():&#160;wypozyczenia.c'],['../wypozyczenia_8c.html#ab4d7bce300ef21148db92a04bf30dfc8',1,'dodaj_wypozyczenie():&#160;wypozyczenia.c']]]
 ];
